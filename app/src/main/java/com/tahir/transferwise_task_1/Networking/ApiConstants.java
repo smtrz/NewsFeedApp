66c010c231d7f5a20900dc01a0a5d8d816e006b5
Tahir Raza <smtrz@yahoo.com>
2019-10-13 02:17:28 +0500

    added retrofit.

---
+++ app/src/main/java/com/tahir/transferwise_task_1/Networking/ApiConstants.java
@@ -0,0 +1,8 @@
+package com.tahir.transferwise_task_1.Networking;
+
+
+import com.tahir.transferwise_task_1.BuildConfig;
+
+public class ApiConstants {
+    public static final String BASE_URL = BuildConfig.BASE_URL;
+}
